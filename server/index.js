68a29ff68747f5dface0b9a9f3bb3d25389a1506
100rabhSG <saurabh.agrawal.1605@gmail.com>
2026-04-13 23:40:32 +0530

    removed exposed credentials

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ app.use(cors({
 app.use('/posts', PostRoute);
 app.use('/user', UserRoute);
 
-// const CONNECTION_URL = 'mongodb+srv://saurabh_sg:saurabh_sg@cluster0.dakyan3.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
